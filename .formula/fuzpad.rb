5f046711238b83f2b601002b6d4f62ed9c3feb11
JianZcar <esteban.jianzcar@outlook.com>
2025-01-26 10:40:35 +0800

    added brew formula

---
+++ .formula/fuzpad.rb
@@ -0,0 +1,24 @@
+class Fuzpad < Formula
+  desc "Minimalistic note management solution. Powered by fzf"
+  homepage "https://github.com/JianZcar/FuzPad"
+  url "https://github.com/JianZcar/FuzPad/releases/download/1.0.3/fuzpad-1.0.3.tar.gz"
+  sha256 "e1959fb4e2dd31726c9bc3cc62ea14eadfa6d5d06ec0b434181804558825d593"
+  license "GPL-3.0-or-later"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  depends_on "bat"
+  depends_on "fzf"
+
+  def install
+    bin.install "bin/fuzpad"
+  end
+
+  test do
+    assert_equal "1", shell_output("#{bin}/fuzpad --test 2>&1 | grep -q \"1\" && echo 1 || echo 0").strip
+  end
+end
+
